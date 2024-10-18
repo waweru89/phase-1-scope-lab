a049f74e57018c66ca29980ceb173e5f3d906af1
MBURU <wawerumburu157@gmail.com>
2024-10-18 18:58:15 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'Mburu';
+ 
+function  getCustomerName(){
+    return customerName;
+}
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Daniel'; 
+}
+
+
+console.log(leastFavoriteCustomer); 
+changeLeastFavoriteCustomer(); 
+
+   
